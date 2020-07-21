e0e83e986b6a2e89fe3f0ecbad1ef9dba45f34b2
Barayuda Gautama <wb-igedebara614600@antfin.com>
2020-07-21 15:41:14 +0800

    nofollow a tag

--- components/profile/Education.js
+++ components/profile/Education.js
@@ -4,7 +4,7 @@ const School = ({ children, href, name, detail }) => (
   <p className="mb-2 md:mb-0">
     <div className="inline-block mr-1">{children}</div>
     <div className="inline-block">
-      <a target="_blank" rel="noopener" href={href} className="link">
+      <a target="_blank" rel="nofollow" href={href} className="link">
         {name}
       </a>
       <em className="small text-muted ml-1">({detail})</em>
