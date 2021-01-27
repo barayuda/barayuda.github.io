082cc1aae45b373ce557a26441cc44e77062e821
Barayuda Gautama <wb-igedebara614600@antfin.com>
2021-01-27 15:26:24 +0800

    update google analytic

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,13 @@ export default class CustomApp extends App {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           ></link>
+
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-31109193-1" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-31109193-1');`,
+            }}
+          />
         </Head>
 
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -16,16 +16,6 @@ class CustomDocument extends Document {
           <Main />
           <NextScript />
           <script src="https://kit.fontawesome.com/5946abd67d.js" crossOrigin="anonymous" async></script>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-31109193-1"></script>
-          <script>
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              
-              gtag('config', 'UA-31109193-1');
-            `}
-          </script>
         </body>
       </Html>
     );
